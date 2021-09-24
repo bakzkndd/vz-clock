7756797c451d76e72be1536d7c19a70e3f3cae7f
bakzkndd <50497725+bakzkndd@users.noreply.github.com>
2021-09-24 14:51:50 +0200

    initial files

---
+++ components/Settings.jsx
@@ -0,0 +1,54 @@
+const { RadioGroup, SwitchItem } = require('@vizality/components/settings');
+const { React } = require('@vizality/webpack');
+
+module.exports = class Settings extends React.Component {
+
+	render() {
+		const { getSetting, updateSetting, toggleSetting } = this.props;
+		return (
+			<div>
+				<RadioGroup
+					onChange={e => {
+						updateSetting('timeFormat', e.value);
+						if (e.value === 1) updateSetting('indicator', false);
+					}}
+					value={getSetting('timeFormat', 0)}
+					options={[
+						{
+							name: '12 hour time',
+							value: 0
+						},
+						{
+							name: '24 hour time',
+							value: 1
+						}
+					]}
+				>
+					Time Format
+				</RadioGroup>
+				<SwitchItem
+					value={getSetting('sticky', false)}
+					onChange={() => toggleSetting('sticky')}
+					note='If the clock should always be visible at the top of the server list.'
+				>
+					Sticky
+				</SwitchItem>
+				<SwitchItem
+					value={getSetting('indicator', false)}
+					onChange={() => toggleSetting('indicator')}
+					disabled={getSetting('timeFormat', 0) !== 0}
+				>
+					AM/PM indicator
+				</SwitchItem>
+				<SwitchItem
+					value={getSetting('twoDigit', false)}
+					onChange={() => toggleSetting('twoDigit')}
+					note='Will add a 0 to the beginning of the hours if less than 10'
+				>
+					Always show two digits
+				</SwitchItem>
+			</div>
+		);
+	}
+
+};
---
+++ index.js
@@ -0,0 +1,37 @@
+const { Plugin } = require('@vizality/entities');
+const { React, getModule } = require('@vizality/webpack');
+const { patch, unpatch } = require('@vizality/patcher');
+const { forceUpdateElement } = require('@vizality/util');
+
+const Clock = require('./components/Clock');
+const Settings = require('./components/Settings');
+
+
+module.exports = class vzclock extends Plugin {
+  async start () {
+    this.injectStyles('./style.css');
+
+    vizality.api.settings.registerSettings('vz-clock-settings', {
+      category: this.entityID,
+      label: 'vz-clock',
+      render: Settings
+    });
+
+    const homeButton  = await getModule([ 'DefaultHomeButton' ]);
+    patch('vz-clock', homeButton, 'DefaultHomeButton', (_, res) => {
+      if (!Array.isArray(res)) res = [ res ];
+      res.unshift(React.createElement(Clock, {
+        className: 'vz-clock sticky',
+        getSetting: this.settings.get,
+        updateSetting: this.settings.update
+      }));
+      return res;
+    });
+  }
+
+  stop () {
+    unpatch('vz-clock');
+    vizality.api.settings.unregisterSettings('vz-clock-settings');
+    forceUpdateElement(`.${(getModule([ 'homeIcon', 'downloadProgress' ], false)).tutorialContainer}`);
+  }
+};
