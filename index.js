0eed85d3dea51cc9520a7d00220de7762798e83d
bakzkndd <50497725+bakzkndd@users.noreply.github.com>
2021-10-13 17:44:49 +0200

    left a log in there (oops)

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ const { patch, unpatch } = require("@vizality/patcher");
 const Clock = require("./components/Clock");
 
 const getDefaultMethodByKeyword = (mdl, keyword) => {
-	const defaultMethod = mdl.__vizalityOriginal_default ?? mdl.default;
-	return typeof defaultMethod === 'function' ? defaultMethod.toString().includes(keyword) : null;
+  const defaultMethod = mdl.__vizalityOriginal_default ?? mdl.default;
+  return typeof defaultMethod === "function"
+    ? defaultMethod.toString().includes(keyword)
+    : null;
 };
 
 module.exports = class vzclock extends Plugin {
   async start() {
     this.injectStyles("./style.css");
 
-    const homeButton = await getModule(m => getDefaultMethodByKeyword(m, 'showDMsOnly'));
+    const homeButton = await getModule((m) =>
+      getDefaultMethodByKeyword(m, "showDMsOnly")
+    );
     patch("vz-clock", homeButton, "default", (_, res) => {
-      console.log(res)
       if (!Array.isArray(res)) res = [res];
       res.unshift(
         React.createElement(Clock, {
