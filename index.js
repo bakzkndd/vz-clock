caef1da1a2efb4ecf54c55c0501c9d9f36280ccf
bakzkndd <50497725+bakzkndd@users.noreply.github.com>
2021-11-03 18:44:20 +0100

    try fixing the clock

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const { patch, unpatch } = require("@vizality/patcher");
 
 const Clock = require("./components/Clock");
 
-const getDefaultMethodByKeyword = (mdl, keyword) => {
-  const defaultMethod = mdl.__vizalityOriginal_default ?? mdl.default;
-  return typeof defaultMethod === "function"
-    ? defaultMethod.toString().includes(keyword)
-    : null;
-};
+// const getDefaultMethodByKeyword = (mdl, keyword) => {
+//   const defaultMethod = mdl.__vizalityOriginal_default ?? mdl.default;
+//   return typeof defaultMethod === "function"
+//     ? defaultMethod.toString().includes(keyword)
+//     : null;
+// };
 
 module.exports = class vzclock extends Plugin {
   async start() {
     this.injectStyles("./style.css");
 
-    const homeButton = await getModule((m) =>
-      getDefaultMethodByKeyword(m, "showDMsOnly")
-    );
-    patch("vz-clock", homeButton, "default", (_, res) => {
+    // const homeButton = await getModule((m) =>
+    //   getDefaultMethodByKeyword(m, "showDMsOnly")
+    // );
+    const homeButton = await getModule(["homeButton"])
+    patch("vz-clock", homeButton, "homeButton", (_, res) => {
       if (!Array.isArray(res)) res = [res];
       res.unshift(
         React.createElement(Clock, {
